23459ac253ed62457cd26f26eb0a4ae85aa36f32
iLazvy <ilazvy@gmail.com>
2025-02-26 18:03:31 -0600

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AcercadeComponent } from './pages/acercade/acercade.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AcercadeComponent
   ],
   imports: [
     BrowserModule,
